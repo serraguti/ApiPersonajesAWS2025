5465c3aa4d7e265d32d8b1eae44a6a80374373d5
serraguti <pacoserranox@gmail.com>
2025-05-13 11:11:07 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Program.cs
@@ -0,0 +1,32 @@
+using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+string connectionString = builder.Configuration.GetConnectionString("MySql");
+builder.Services.AddTransient<RepositoryPersonajes>();
+builder.Services.AddDbContext<PersonajesContext>
+    (options => options.UseMySQL(connectionString));
+builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+app.MapScalarApiReference(opt =>
+{
+    opt.Title = "Scalar Personajes";
+    opt.Theme = ScalarTheme.BluePlanet;
+});
+app.MapOpenApi();
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
+
